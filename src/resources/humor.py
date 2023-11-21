dbb201ced28dc7d66ca5e0ff68d4b3a157ee79bb
Vinicius Peixoto <vinicius.opeixoto@gmail.com>
2023-11-21 09:29:44 -0300

    Added logging to the API

--- src/resources/humor.py
+++ src/resources/humor.py
@@ -1,4 +1,6 @@
 import json
+import logging
+import logging.config
 from datetime import datetime
 
 import falcon
@@ -7,52 +9,72 @@ from src.repository.models import Humor
 from src.resources.base import Resource
 
 
+logging.config.fileConfig("src/utils/logging.conf")
+simpleLogger = logging.getLogger("simpleLogger")
+detailedLogger = logging.getLogger("detailedLogger")
+
+
 class HumorResource(Resource):
     def on_get(self, req: falcon.Request, resp: falcon.Response, humor_id: int):
+        simpleLogger.info(f"GET /humor/{humor_id}")
         humor = None
         try:
+            simpleLogger.debug("Fetching humor from database using id.")
             humor = self.uow.repository.get_humor_by_id(humor_id)
             self.uow.commit()
         except Exception as e:
-            resp.body = json.dumps({"exception": e.__str__()})
+            detailedLogger.error("Could not perform fetch humor database operation!", exc_info=True)
+            resp.body = json.dumps({"error": "The server could not fetch the humor."})
             resp.status = falcon.HTTP_INTERNAL_SERVER_ERROR
 
         if not humor:
-            resp.body = json.dumps({"error": f"No Humor data with id {humor_id}"})
+            simpleLogger.debug(f"No Humor data with id {humor_id}.")
+            resp.body = json.dumps({"error": f"No Humor data with id {humor_id}."})
             resp.status = falcon.HTTP_NOT_FOUND
             return
 
         resp.text = json.dumps(json.loads(str(humor)))
         resp.status = falcon.HTTP_OK
+        simpleLogger.info(f"GET /humor/{humor_id} : successful")
 
     def on_get_date(self, req: falcon.Request, resp: falcon.Response, humor_date: str):
+        simpleLogger.info(f"GET /humor/date/{humor_date}")
         humor = None
         try:
+            simpleLogger.debug("Formatting the date for humor.")
             humor_date = datetime.strptime(humor_date, "%Y-%m-%d").date()
         except Exception as e:
-            resp.body = json.dumps({"exception": e.__str__()})
+            detailedLogger.warning(f"Date {humor_date} is malformed!", exc_info=True)
+            resp.body = json.dumps({"error": f"Date {humor_date} is malformed! Correct format is YYYY-MM-DD."})
             resp.status = falcon.HTTP_BAD_REQUEST
             return
         try:
+            simpleLogger.debug("Fetching humor from database using date.")
             humor = self.uow.repository.get_humor_by_date(humor_date)
             self.uow.commit()
         except Exception as e:
-            resp.body = json.dumps({"exception": e.__str__()})
+            detailedLogger.error("Could not perform fetch humor database operation!", exc_info=True)
+            resp.body = json.dumps({"error": "The server could not fetch the humor."})
             resp.status = falcon.HTTP_INTERNAL_SERVER_ERROR
+            return
 
         if not humor:
-            resp.body = json.dumps({"error": f"No Humor data in date {humor_date}"})
+            simpleLogger.debug(f"No Humor data in date {humor_date}.")
+            resp.body = json.dumps({"error": f"No Humor data in date {humor_date}."})
             resp.status = falcon.HTTP_NOT_FOUND
             return
 
         resp.text = json.dumps(json.loads(str(humor)))
         resp.status = falcon.HTTP_OK
+        simpleLogger.info(f"GET /humor/date/{humor_date} : successful")
 
     def on_post_add(self, req: falcon.Request, resp: falcon.Response):
+        simpleLogger.info("POST /humor")
         body = req.stream.read(req.content_length or 0)
         body = json.loads(body.decode("utf-8"))
         if not body:
-            resp.body = json.dumps({"error": "Missing request body."})
+            simpleLogger.debug("Missing request body for humor.")
+            resp.body = json.dumps({"error": "Missing request body for humor."})
             resp.status = falcon.HTTP_BAD_REQUEST
             return
         humor_value = body.get("value")
@@ -60,6 +82,7 @@ class HumorResource(Resource):
         humor_health_based = body.get("health_based")
 
         if not all((humor_value, humor_description, humor_health_based)):
+            simpleLogger.debug("Missing Humor parameter.")
             resp.body = json.dumps({"error": "Missing Humor parameter."})
             resp.status = falcon.HTTP_BAD_REQUEST
             return
@@ -69,11 +92,14 @@ class HumorResource(Resource):
             health_based=humor_health_based == "True",
         )
         try:
+            simpleLogger.debug("Trying to add Humor data to database.")
             self.uow.repository.add_mood(humor)
             self.uow.commit()
         except Exception as e:
-            resp.body = json.dumps({"exception": e.__str__()})
+            detailedLogger.error("Could not perform add humor to database operation!", exc_info=True)
+            resp.body = json.dumps({"error": "The server could not add the humor."})
             resp.status = falcon.HTTP_INTERNAL_SERVER_ERROR
             return
 
         resp.status = falcon.HTTP_CREATED
+        simpleLogger.info("POST /humor : successful")
--- src/resources/water.py
+++ src/resources/water.py
@@ -1,4 +1,6 @@
 import json
+import logging
+import logging.config
 from datetime import datetime
 
 import falcon
@@ -7,60 +9,76 @@ from src.repository.models import Water
 from src.resources.base import Resource
 
 
+logging.config.fileConfig("src/utils/logging.conf")
+simpleLogger = logging.getLogger("simpleLogger")
+detailedLogger = logging.getLogger("detailedLogger")
+
+
 class WaterResource(Resource):
     def on_get(self, req: falcon.Request, resp: falcon.Response, water_intake_id: int):
+        simpleLogger.info(f"GET /water-intake/{water_intake_id}")
         water_intake = None
         try:
+            simpleLogger.debug("Fetching water intake from database using id.")
             water_intake = self.uow.repository.get_water_intake_by_id(water_intake_id)
             self.uow.commit()
         except Exception as e:
-            resp.body = json.dumps({"exception": e.__str__()})
+            detailedLogger.error("Could not perform fetch water intake database operation!", exc_info=True)
+            resp.body = json.dumps({"error": "The server could not fetch the water intake."})
             resp.status = falcon.HTTP_INTERNAL_SERVER_ERROR
 
         if not water_intake:
-            resp.body = json.dumps(
-                {"error": f"No Water Intake data with id {water_intake_id}"}
-            )
+            simpleLogger.debug(f"No Water data with id {water_intake_id}.")
+            resp.body = json.dumps({"error": f"No Water data with id {water_intake_id}."})
             resp.status = falcon.HTTP_NOT_FOUND
             return
 
         resp.text = json.dumps(json.loads(str(water_intake)))
         resp.status = falcon.HTTP_OK
+        simpleLogger.info(f"GET /water-intake/{water_intake_id} : successful")
 
     def on_get_date(
         self, req: falcon.Request, resp: falcon.Response, water_intake_date: str
     ):
+        simpleLogger.info(f"GET /water-intake/date/{water_intake_date}")
         water_intake = None
         try:
+            simpleLogger.debug("Formatting the date for water intake.")
             water_intake_date = datetime.strptime(water_intake_date, "%Y-%m-%d").date()
         except Exception as e:
-            resp.body = json.dumps({"exception": e.__str__()})
+            detailedLogger.warning(f"Date {water_intake_date} is malformed!", exc_info=True)
+            resp.body = json.dumps({"error": f"Date {water_intake_date} is malformed! Correct format is YYYY-MM-DD."})
             resp.status = falcon.HTTP_BAD_REQUEST
             return
         try:
+            simpleLogger.debug("Fetching water intake from database using date.")
             water_intake = self.uow.repository.get_water_intake_by_date(
                 water_intake_date
             )
             self.uow.commit()
         except Exception as e:
-            resp.body = json.dumps({"exception": e.__str__()})
+            detailedLogger.error("Could not perform fetch water intake database operation!", exc_info=True)
+            resp.body = json.dumps({"error": "The server could not fetch the water intake."})
             resp.status = falcon.HTTP_INTERNAL_SERVER_ERROR
+            return
 
         if not water_intake:
-            resp.body = json.dumps(
-                {"error": f"No Water Intake data in date {water_intake_date}"}
-            )
+            simpleLogger.debug(f"No Water data in date {water_intake_date}.")
+            resp.body = json.dumps({"error": f"No Water data in date {water_intake_date}."})
             resp.status = falcon.HTTP_NOT_FOUND
             return
 
         resp.text = json.dumps(json.loads(str(water_intake)))
         resp.status = falcon.HTTP_OK
+        simpleLogger.info(f"GET /water-intake/date/{water_intake_date} : successful")
 
     def on_post_add(self, req: falcon.Request, resp: falcon.Response):
+        simpleLogger.info("POST /water-intake")
         body = req.stream.read(req.content_length or 0)
         body = json.loads(body.decode("utf-8"))
         if not body:
-            resp.body = json.dumps({"error": "Missing request body."})
+            simpleLogger.debug("Missing request body for water intake.")
+            resp.body = json.dumps({"error": "Missing request body for water intake."})
             resp.status = falcon.HTTP_BAD_REQUEST
             return
         water_intake_ml = body.get("milliliters")
@@ -68,7 +86,8 @@ class WaterResource(Resource):
         water_intake_pee = body.get("pee")
 
         if not all((water_intake_ml, water_intake_description, water_intake_pee)):
-            resp.body = json.dumps({"error": "Missing Water Intake parameter."})
+            simpleLogger.debug("Missing Water parameter.")
+            resp.body = json.dumps({"error": "Missing Water parameter."})
             resp.status = falcon.HTTP_BAD_REQUEST
             return
         water_intake = Water(
@@ -77,11 +96,14 @@ class WaterResource(Resource):
             pee=water_intake_pee == "True",
         )
         try:
+            simpleLogger.debug("Trying to add Water data to database.")
             self.uow.repository.add_water_intake(water_intake)
             self.uow.commit()
         except Exception as e:
-            resp.body = json.dumps({"exception": e.__str__()})
+            detailedLogger.error("Could not perform add water intake to database operation!", exc_info=True)
+            resp.body = json.dumps({"error": "The server could not add the water intake."})
             resp.status = falcon.HTTP_INTERNAL_SERVER_ERROR
             return
 
         resp.status = falcon.HTTP_CREATED
+        simpleLogger.info("POST /water-intake: successful")
